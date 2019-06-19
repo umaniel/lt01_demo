3425101d5cdda01765409cfffc7cb0993cf097a4
UMA <yuma.muroya@phoneappli.net>
2019-06-19 14:42:00 +0900

    php fix

--- index.php
+++ index.php
@@ -1,23 +1,2 @@
-<?php
 
-function require_auth() {
-    $AUTH_USER = 'demo';
-    $AUTH_PASS = '190620';
-
-    header('Cache-Control: no-cache, must-revalidate, max-age=0');
-    $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
-    $is_not_authenticated = (
-        !$has_supplied_credentials ||
-        $_SERVER['PHP_AUTH_USER'] != $AUTH_USER ||
-        $_SERVER['PHP_AUTH_PW']   != $AUTH_PASS
-    );
-    if ($is_not_authenticated) {
-        header('HTTP/1.1 401 Authorization Required');
-        header('WWW-Authenticate: Basic realm="Access denied"');
-        exit;
-    }
-}
-
-require_auth();
-
-header('Location: /index.html');
+<?php include_once("index.html"); ?>
