1acc5924af6bd0cc8691e08679640169f0016ab1
UMA <yuma.muroya@phoneappli.net>
2019-06-19 14:44:27 +0900

    basic

--- index.php
+++ index.php
@@ -1,2 +1,24 @@
+<?php
 
-<?php include_once("index.html"); ?>
+function require_auth() {
+    $AUTH_USER = 'admin';
+    $AUTH_PASS = 'admin';
+
+    header('Cache-Control: no-cache, must-revalidate, max-age=0');
+    $has_supplied_credentials = !(empty($_SERVER['PHP_AUTH_USER']) && empty($_SERVER['PHP_AUTH_PW']));
+    $is_not_authenticated = (
+        !$has_supplied_credentials ||
+        $_SERVER['PHP_AUTH_USER'] != $AUTH_USER ||
+        $_SERVER['PHP_AUTH_PW']   != $AUTH_PASS
+    );
+    if ($is_not_authenticated) {
+        header('HTTP/1.1 401 Authorization Required');
+        header('WWW-Authenticate: Basic realm="Access denied"');
+        exit;
+    }
+}
+
+require_auth();
+
+
+include_once("index.html"); ?>
